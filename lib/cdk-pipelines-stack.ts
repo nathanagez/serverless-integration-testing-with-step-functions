c5cee20d67fb38e922a7eb601553b0a0b4a5f47d
Nathan <nathan.agez@epitech.eu>
2022-01-07 18:01:17 +0100

    feat(pipeline): add pipeline construct

--- lib/cdk-pipelines-stack.ts
+++ lib/cdk-pipelines-stack.ts
@@ -1,16 +1,18 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
-export class CdkPipelinesStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelinesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        // TODO: replace values with environment variables
+        input: CodePipelineSource.gitHub('nathanagez/serverless-integration-testing-with-step-functions', 'master'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
